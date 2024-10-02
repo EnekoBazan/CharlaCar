83c70e88606566db02ca640dcfedefb3a1f9ba53
EnekoBazan <eneko.bazan@opendeusto.es>
2024-10-02 11:30:18 +0200

    rediseñada ventana principal

--- CharlaCar/src/guiLP/VentanaPrincipal.java
+++ CharlaCar/src/guiLP/VentanaPrincipal.java
@@ -4,6 +4,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -36,9 +37,17 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 		
 		PanelPrincipal = new JPanel();
 		PanelPrincipal.setLayout(new GridLayout(2, 2, 10, 10));
+		PanelPrincipal.setBorder(BorderFactory.createTitledBorder("CharlaCar"));
+		
+		JPanel blanc = new JPanel();
+		PanelPrincipal.add(blanc);
 		
 		btnCrearViaje = new JButton("Crear Viaje");
 		PanelPrincipal.add(btnCrearViaje);
+		
+		
+		JPanel blanc1 = new JPanel();
+		PanelPrincipal.add(blanc1);
 		
 		btnBuscarViaje = new JButton("Buscar Viaje");
 		PanelPrincipal.add(btnBuscarViaje);
